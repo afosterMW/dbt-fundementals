64643635a9738f39d5dca1301e490dfadbdb533b
alexander.foster@mavenwave.com <alexander.foster@mavenwave.com>
2022-01-17 17:46:28 +0000

    configured yml and testing files

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,13 +1,10 @@
-with payments as (
+select
+    orderid as order_id,
+    id as payment_id,
+    paymentmethod as payment_method,
+    status,
+    amount / 100 as amount,
+    created as created_at
 
-    select
-        orderid as order_id,
-        id as payment_id,
-        paymentmethod as payment_method,
-        status,
-        amount / 100 as amount,
-        created as created_at
-
-    from `dbt-tutorial.stripe.payment`
-)
-select * from payments
+-- from `dbt-tutorial.stripe.payment`
+from {{ source('stripe', 'payment') }}
